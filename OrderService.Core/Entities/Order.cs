ddb6ea26fb6f7b933c939ea46423784dfbac9131
Eva-Lotta Karlsson <eva.lotta.karlsson@stendahls.se>
2024-12-16 14:48:19 +0100

    Removed comments

--- OrderService.Core/Entities/Order.cs
+++ OrderService.Core/Entities/Order.cs
@@ -5,7 +5,7 @@ public class Order
 
 	public int Id { get; set; }
 	public string CustomerName { get; set; }
-	public List<int> PizzaIds { get; set; } // Foreign keys till Pizza Service.
-	public string Status { get; set; } = "Pending"; // default värde
+	public List<int> PizzaIds { get; set; }
+	public string Status { get; set; } = "Pending";
 	public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
 }
